3da00e152076474a8ec649724a1f4b0737d1dba0
Benoit <benoit.colin@web-atrio.com>
2017-03-14 14:33:22 +0100

    initial commit

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+    /**
+     * @Rest\View
+     * @Rest\Get("/users")
+     */
+    public function getUsersAction(Request $request)
+    {
+        $users = $this->get('doctrine.orm.entity_manager')
+                ->getRepository('AppBundle:User')
+                ->findAll();
+
+        return $users;
+    }
+}    
